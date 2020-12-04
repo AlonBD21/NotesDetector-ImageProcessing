b3293d1f01bb6589f04fe0d0abb6ad3d54464bbf
Alon Ben Dov <alonbd21@gmail.com>
2020-12-04 17:14:27 +0200

    Day 0
    good job

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-
-
 from __future__ import print_function
 import cv2 as cv
 import numpy as np
 import argparse
 import random as rng
-rng.seed(12345)
 
+rng.seed(12345)
 
 # Load source image
 parser = argparse.ArgumentParser(description='Code for Finding contours in your image tutorial.')
@@ -18,20 +16,23 @@ if src is None:
     exit(0)
 # Convert image to gray and blur it
 src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-src_gray = cv.blur(src_gray, (3,3))
+src_gray = cv.blur(src_gray, (3, 3))
 # Detect edges using Canny
 canny_output = cv.Canny(src_gray, 100, 100 * 2)
 # Find contours
-contours, hierarchy = cv.findContours(canny_output, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-[hierarchy[:,3].argsort()]
-print(hierarchy)
+
+# Manipulate Source
+contours, hierarchy = cv.findContours(canny_output, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
+hierarchy = hierarchy[0]
+
+hierarchy = hierarchy[np.argsort(hierarchy[:, 3])]
+print(len(contours))
+
+for i,contour in enumerate(contours):
+    found = cv.putText(src,str(i),(contour[0][0][0]-30,contour[0][0][1]),cv.FONT_HERSHEY_TRIPLEX,1,(255,0,0))
 
 
-
-cv.imshow("Canny",canny_output)
-
-
-
+cv.imshow("Found",found)
 
 
 cv.waitKey()
