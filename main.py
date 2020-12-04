0ca421dccc463ae819fba39b7d866303c9e384cc
Alon Ben Dov <alonbd21@gmail.com>
2020-12-04 16:29:12 +0200

    16:29

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
-import numpy as np
+
+
+from __future__ import print_function
 import cv2 as cv
+import numpy as np
+import argparse
+import random as rng
+rng.seed(12345)
 
 
-if __name__ == '__main__':
-    pass
+# Load source image
+parser = argparse.ArgumentParser(description='Code for Finding contours in your image tutorial.')
+parser.add_argument('--input', help='Path to input image.', default='image.png')
+args = parser.parse_args()
+src = cv.imread(cv.samples.findFile(args.input))
+if src is None:
+    print('Could not open or find the image:', args.input)
+    exit(0)
+# Convert image to gray and blur it
+src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+src_gray = cv.blur(src_gray, (3,3))
+# Detect edges using Canny
+canny_output = cv.Canny(src_gray, 100, 100 * 2)
+# Find contours
+contours, hierarchy = cv.findContours(canny_output, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+[hierarchy[:,3].argsort()]
+print(hierarchy)
 
+
+
+cv.imshow("Canny",canny_output)
+
+
+
+
+
+cv.waitKey()
