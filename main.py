962a34e6b1348e1de9225934caa1aa961e1711e9
Alon Ben Dov <alonbd21@gmail.com>
2020-12-14 22:41:22 +0200

    modulated code

--- main.py
+++ main.py
@@ -1,58 +1,70 @@
-from __future__ import print_function
 import cv2 as cv
 import numpy as np
-import argparse
-import random as rng
+
+# drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
+
+MEAN_THRESH = 70
+CONTOUR_LENGTH_THRESH = 20
 
 
-src = cv.imread(cv.samples.findFile("image.png"))
-# Convert image to gray and blur it
-src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-copy = src_gray.copy()
-src_gray = cv.blur(src_gray, (2, 2))
-cv.threshold(src_gray, 220, 255, cv.THRESH_BINARY)
-# Detect edges using Canny
-canny_output_lines = cv.Canny(src_gray, 100, 200)
-canny_output = canny_output_lines.copy()
-drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
-contours, hierarchy = cv.findContours(canny_output, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+def load_image_gray(address):
+    src = cv.imread(cv.samples.findFile(address))
+    src = cv.resize(src, (int(src.shape[1] * 1.2), int(src.shape[0] * 1.2)))
+    return src
 
-# Find contours
-lines = cv.HoughLinesP(canny_output, 1, np.pi / 180, 40, minLineLength=30)
-for d in lines:
-    x1, y1, x2, y2 = d[0][0], d[0][1], d[0][2], d[0][3]
-    cv.line(canny_output_lines, (x1, y1), (x2, y2), (0, 0, 0), 2)
-contours_lines, hierarchy_lines = cv.findContours(canny_output_lines, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-contours, hierarchy = cv.findContours(canny_output, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-mask_base = np.zeros(src_gray.shape, dtype="uint8")
-contour_weight = []
-for contour in contours:
-    i = 0
-    mask = mask_base.copy()
-    mask = cv.drawContours(mask, [contour], -1, (255))
-    mask = cv.fillPoly(mask, [contour], 255)
-    mean = cv.mean(copy, mask=mask)
-    if mean[0] > 70:
-        string = "half"
-        cv.fillPoly(src, pts=[contour], color=(0, 255, 0))
-        contour_weight.append((contour, string))
-    else:
-        x, y, w, h = cv.boundingRect(contour)
-        cropped = canny_output_lines[y:y + h, x:x + w]
-        temp_contours, temp_hierarchy = cv.findContours(cropped, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-        i = 0
-        for con in temp_contours:
-            if cv.arcLength(con, False) > 20:
-             i += 1
-        if i > 1:
-            string = "eighth"
-            cv.fillPoly(src, pts=[contour], color=(255, 0, 0))
+
+def initial_manipulation(img):
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    ret, img = cv.threshold(img, 200, 255, cv.THRESH_BINARY)
+    # kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (9, 9))
+    # img = cv.erode(img,kernel)
+    return img
+
+
+def canny_algorithm(img):
+    canny = cv.Canny(img, 100, 200)
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
+    canny = cv.dilate(canny,kernel)
+    return canny
+
+
+def find_contours(canny):
+    contours, hierarchy = cv.findContours(canny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    return contours
+
+
+def main():
+    src = load_image_gray("image.png")
+    original = src.copy()
+    src = initial_manipulation(src)
+    src_copy = src.copy()
+
+    canny = canny_algorithm(src)
+    ext_contours = find_contours(canny)
+
+
+    cv.drawContours(original, ext_contours, -1, (0, 255, 0))
+    for contour in ext_contours:
+        mean = mean_of_contour(src, contour)[0]  # Channel 0
+        if mean > MEAN_THRESH:
+            string = "half"
         else:
-            string = "quarter"
-            cv.fillPoly(src, pts=[contour], color=(0, 0, 255))
-        contour_weight.append((contour, string))
-    src = cv.putText(src, string, (contour[0][0][0] - 30, contour[0][0][1]),
-                       cv.FONT_HERSHEY_TRIPLEX, 1, (255, 0, 0))
-cv.imshow("Found", src)
-cv.waitKey()
+            string = "eighth or quarter"
 
+        cv.putText(original, string+" "+str(mean)[:4], (contour[0][0][0] - 100, contour[0][0][1]),
+                          cv.FONT_HERSHEY_DUPLEX, 0.5, (100, 200, 0))
+    cv.imshow("Found", original)
+    cv.imshow("canny",canny)
+    cv.imshow("src",src)
+    cv.waitKey()
+
+
+def mean_of_contour(src, contour):
+    mask = np.zeros(src.shape, dtype="uint8")
+    mask = cv.fillPoly(mask, [contour], 255)
+    return cv.mean(src, mask=mask)
+
+
+
+if __name__ == "__main__":
+    main()
--- temp.py
+++ temp.py
@@ -1,52 +1,57 @@
-
-
 from __future__ import print_function
 import cv2 as cv
 import numpy as np
 import argparse
 import random as rng
-rng.seed(12345)
 
-def thresh_callback(val):
-    threshold = val
-    # Detect edges using Canny
-    canny_output = cv.Canny(src_gray, threshold, threshold * 2)
-    # Find contours
-    contours, hierarchy = cv.findContours(canny_output, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    # Draw contours
-    drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
-    for i in range(len(contours)):
-        color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
-        cv.drawContours(drawing, contours, i, color, 2, cv.LINE_8, hierarchy, 0)
-    # Show in a window
-    cv.imshow('Contours', drawing)
-    return contours
-# Load source image
-parser = argparse.ArgumentParser(description='Code for Finding contours in your image tutorial.')
-parser.add_argument('--input', help='Path to input image.', default='image.png')
-args = parser.parse_args()
-src = cv.imread(cv.samples.findFile(args.input))
-if src is None:
-    print('Could not open or find the image:', args.input)
-    exit(0)
+
+src = cv.imread(cv.samples.findFile("image.png"))
 # Convert image to gray and blur it
 src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-src_gray = cv.blur(src_gray, (3,3))
-# Create Window
-source_window = 'Source'
-cv.namedWindow(source_window)
-cv.imshow(source_window, src)
-max_thresh = 255
-thresh = 100 # initial threshold
-cv.createTrackbar('Canny Thresh:', source_window, thresh, max_thresh, thresh_callback)
-con = thresh_callback(thresh)
+copy = src_gray.copy()
+src_gray = cv.blur(src_gray, (2, 2))
+cv.threshold(src_gray, 220, 255, cv.THRESH_BINARY)
+# Detect edges using Canny
+canny_output_lines = cv.Canny(src_gray, 100, 200)
+canny_output = canny_output_lines.copy()
+drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
+contours, hierarchy = cv.findContours(canny_output, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
-print(len(con))
-
-
-
-
-
-
-
+# Find contours
+lines = cv.HoughLinesP(canny_output, 1, np.pi / 180, 40, minLineLength=30)
+for d in lines:
+    x1, y1, x2, y2 = d[0][0], d[0][1], d[0][2], d[0][3]
+    cv.line(canny_output_lines, (x1, y1), (x2, y2), (0, 0, 0), 2)
+contours_lines, hierarchy_lines = cv.findContours(canny_output_lines, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv.findContours(canny_output, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+mask_base = np.zeros(src_gray.shape, dtype="uint8")
+contour_weight = []
+for contour in contours:
+    i = 0
+    mask = mask_base.copy()
+    mask = cv.drawContours(mask, [contour], -1, (255))
+    mask = cv.fillPoly(mask, [contour], 255)
+    mean = cv.mean(copy, mask=mask)
+    if mean[0] > 70:
+        string = "half"
+        cv.fillPoly(src, pts=[contour], color=(0, 255, 0))
+        contour_weight.append((contour, string))
+    else:
+        x, y, w, h = cv.boundingRect(contour)
+        cropped = canny_output_lines[y:y + h, x:x + w]
+        temp_contours, temp_hierarchy = cv.findContours(cropped, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        i = 0
+        for con in temp_contours:
+            if cv.arcLength(con, False) > 20:
+             i += 1
+        if i > 1:
+            string = "eighth"
+            cv.fillPoly(src, pts=[contour], color=(255, 0, 0))
+        else:
+            string = "quarter"
+            cv.fillPoly(src, pts=[contour], color=(0, 0, 255))
+        contour_weight.append((contour, string))
+    src = cv.putText(src, string, (contour[0][0][0] - 30, contour[0][0][1]),
+                       cv.FONT_HERSHEY_TRIPLEX, 1, (255, 0, 0))
+cv.imshow("Found", src)
 cv.waitKey()
